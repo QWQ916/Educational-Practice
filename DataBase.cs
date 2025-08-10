10b91d6b4e7f5dc70d8e96439d86172fedb0964e
QWQ916 <Shirok2005@icloud.com>
2025-08-10 23:13:43 +0300

    Добавлены исходные коды 6 классов

---
+++ DataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SportCLUB
+{
+    public class DataBase
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-5TBAK58Q;Initial Catalog=SportClub;Integrated Security=True");
+        public void OpenCon()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        public void CloseCon()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        public SqlConnection GetConnection()
+        {
+            return con;
+        }
+    }
+}
